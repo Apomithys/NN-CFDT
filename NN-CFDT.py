e6d1e0eb4e6033f0a6fb57e956090c22ab060979
PassiSchoppi <40070838+PassiSchoppi@users.noreply.github.com>
2018-09-16 16:31:48 +0200

    funktioniert Kommentare: NN

--- NN-CFDT.py
+++ NN-CFDT.py
@@ -2,23 +2,39 @@
 print("start test.py")
 
 #Matrixmultiplikation layer * weights mit layer hat länge Lfron und gibt layerB mit Lto länge aus
-def matmult(layer, weights, Lfrom, Lto):
-    layerB = [0, 0]
-    for i in range(0, Lfrom):
-        for o in range(0, Lto):
-            layerB[o] = layerB[o] + (layer[i] * weights[o][i])
-    return(layerB)
+def matmult(MMlayer, MMweights, MMLfrom, MMLto):
+    MMlayerOut = [0, 0]
+    for i in range(0, MMLfrom):
+        for o in range(0, MMLto):
+            MMlayerOut[o] = MMlayerOut[o] + (MMlayer[i] * MMweights[o][i])
+    return(MMlayerOut)
     #Status
     print("Matrixmultiplikation erfolgreich berechnet")
 
-#eingabelayer
+# def NN(ENNlayer0, ENNweigths0, ENNweights1, ENNweights2):
+#     NNlayer0 = ENNlayer0
+#     NNlayer1 = [0, 0, 0]
+#     NNlayer2 = [0, 0]
+#     NNlayerOut = [0]
+#     NNweights0 = ENNweigths0
+#     NNweights1 = ENNweights1
+#     NNweights2 = ENNweights2
+#     NNlayer1 = matmult(NNlayer0, NNweights0, 3, 3)
+#     NNlayer2 = matmult(NNlayer1, NNweights1, 3, 2)
+#     NNlayerOut = matmult(NNlayer2, NNweights2, 2, 1)
+#     return(NNlayerOut)
+
 a = int(input("0: "))
 b = int(input("1: "))
 c = int(input("2: "))
 layer0 = [ a, b, c]
 
 #gewichtungen zwischen layer0 und layer1
-weights0 = [ 3, 3, 3],[ 4, 4, 4]
+weights0 = [3, 3, 3], [4, 4, 4]
+# weights1 = [1, 1, 1], [2, 2, 2]
+# weights2 = [5, 5]
+# layerOut = NN(layer0, weights0, weights1, weights2)
 
 #Berechnung des usgabelayers
 layer1 = matmult(layer0, weights0, 3, 2)
+print(layer1)
