9b6e460fc8a3c15e4330acba20cc5942dad18875
PassiSchoppi <40070838+PassiSchoppi@users.noreply.github.com>
2018-10-04 16:24:07 +0200

    -0.01 bis 0.01 muss noch in NN-CFDT.py eingearbeitet werden

--- randomizeNN.py
+++ randomizeNN.py
@@ -1,12 +1,11 @@
 import random
-
 import csv
 
-with open(str(input("which file: ")), 'w') as file:
+with open(str(input("which file: ")), 'w', newline='') as file:
     thewriter = csv.writer(file)
 
     for o in range(0,100):
         row = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         for i in range(0,9):
-            row[i] = random.uniform(-1, 1)
+            row[i] = float(random.uniform(-0.01, 0.01))
         thewriter.writerow(row)
