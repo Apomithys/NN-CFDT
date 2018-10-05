728bd04930e27cea02b44e8fd200975a807cd3ad
PassiSchoppi <40070838+PassiSchoppi@users.noreply.github.com>
2018-10-05 11:54:07 +0200

    eingabe von Tabellengröße

--- randomizeNN.py
+++ randomizeNN.py
@@ -3,9 +3,10 @@ import csv
 
 with open(str(input("which file: ")), 'w', newline='') as file:
     thewriter = csv.writer(file)
-
-    for o in range(0,100):
+    howrow = int(input("how many rows: "))
+    howcoul = int(input("how many columns: "))
+    for o in range(0, howrow):
         row = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        for i in range(0,9):
+        for i in range(0, howcoul):
             row[i] = float(random.uniform(-0.01, 0.01))
         thewriter.writerow(row)
