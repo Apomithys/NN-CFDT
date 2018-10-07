00c7826fbd854f4b67262133d480ca23189297fa
PassiSchoppi <40070838+PassiSchoppi@users.noreply.github.com>
2018-10-07 13:06:25 +0200

    alles in funktionen

---
+++ NN_CFDT.py
@@ -0,0 +1,73 @@
+#import stuff
+import random
+from random import randint
+import csv
+
+########################################################    Funktions
+
+#Einlesen der Tabelle
+def readTable(name):
+    r = csv.reader(open(name))
+    lines = list(r)
+    return(lines)
+
+#seperate the data für das Netztwerk als output
+def seperateData(tabelle, t):
+    data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    counter = 0
+    for p in range(t-10, t):
+        data[counter] = tabelle[p][0]
+        counter = counter + 1
+    return(data)
+
+########################################################    Network funktion
+
+#matrixmultiplikation speziell für NN-Gewichtungen
+def layermalweights(layerIn, NN, index):
+    layerOut = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(0,9):
+        for o in range(0,9):
+            layerOut[i] = layerOut[i] + (float(layerIn[i]) * float(NN[o+index][i]))
+    return(layerOut)
+
+#Funktion mit einem Eingabelayer die einen Wert nach NN
+def NNrechner(layerIn, weightsIn):
+    #kann man das auch als for schleife machen ???
+    layer1 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    layer2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    layer3 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    layer4 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    layer5 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    layer1 = layermalweights(layerIn, weightsIn, 0)
+    layer2 = layermalweights(layer1, weightsIn, 10)
+    layer3 = layermalweights(layer2, weightsIn, 20)
+    layer4 = layermalweights(layer3, weightsIn, 30)
+    layer5 = layermalweights(layer4, weightsIn, 40)
+
+    layerOut = 0
+    for i in range(0, len(layer5)):
+        layerOut = layerOut + layer5[i]
+    return(layerOut)
+
+########################################################    Main part
+
+def triffVoraussage():
+    #Einlesen der Tabelle "kurs.csv" als kurs
+    kurs = []
+    kurs = readTable(str(input("coursetable: ")))
+
+    #Einlesender Tabelle "NN.csv" als NN
+    NN = []
+    NN = readTable(str(input("Neural Network: ")))
+
+    time = int(input("time(type'10' to see latest): "))
+
+    #sichtbare Daten für das NN als Eingabelayer
+    daten = []
+    daten = seperateData(kurs, time)
+    print("the NN sees: " + str(daten))
+
+    #Berechnung des wettabfalls
+    einsatzvor = NNrechner(daten, NN)
+    print("you should bet: " + str(einsatzvor))
